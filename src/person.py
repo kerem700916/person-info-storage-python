61cfde7f759b166d0b5f4d6d2cf25bd9224b2f4c
KeremGuven <guvenkerem2006@gmail.com>
2021-08-16 11:51:28 +0300

    Create person.py

---
+++ src/person.py
@@ -0,0 +1,49 @@
+import pandas, json
+
+class person():
+    def __init__(self, name, age, sex, country, dad_name, mom_name, job, email):
+        global data_frame
+        self.name = name
+        self.age = age
+        self.sex = sex
+        self.country = country
+        self.dad_name = dad_name
+        self.mom_name = mom_name
+        self.job = job
+        self.email = email
+        data_frame = pandas.DataFrame(
+            ['name', 'age','sex', 'country', 'dad_name', 'mom_name', 'job', 'email'],
+            [name, age, sex, country, dad_name, mom_name, job, email]
+        )
+        file_name = self.name + '.json'
+        file = open(file_name, 'a')
+        file_read = open(file_name, 'r')
+        content = file_read.read()
+        file.truncate(0)
+        string = '''{
+    "'''+self.name+'''": [
+        {
+            "name":"'''+self.name+'''",
+            "age":"'''+self.age+'''",
+            "sex":"'''+self.sex+'''",
+            "country":"'''+self.country+'''",
+            "dad_name":"'''+self.dad_name+'''",
+            "mom_name":"'''+self.mom_name+'''",
+            "job":"'''+self.job+'''",
+            "email":"'''+self.email+'''"
+        }
+    ]
+}\n'''
+        file.write(string)
+        file.close()
+
+def get_info(name):
+    info = []
+    with open(name+'.json') as json_file:
+        data = json.load(json_file)
+    for d in data[name]:
+        info.append(d)
+    return info
+
+jack = person(name='kerem', age='20', sex='male', country='en', dad_name='mike', mom_name='helen', job='student', email='jackmail@gmail.com')
+print(get_info('kerem'))
